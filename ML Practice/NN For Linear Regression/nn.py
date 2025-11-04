efbc195cc3ffca2cbb085a4de6c662918c42aba2
bryanarroyo01 <barroyo-fernandez@outlook.com>
2025-11-04 06:15:08 -0700

    Practice Folder Pytorch

---
+++ ML Practice/NN For Linear Regression/nn.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+
+
+class myNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(myNN, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
+
+
+def main():
+    nn_input_size = 2  # Input size (x, y)
+    nn_hidden_size = 2  # I am looking for m and b
+
+
+if __name__ == "__main__":
+    main()    # Define a simple neural network model and practrice how to train it using Pytorch
