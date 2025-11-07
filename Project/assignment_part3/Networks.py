87bf9b2de6776ca0d605ad0f8f74d5559e964ff8
bryanarroyo01 <barroyo-fernandez@outlook.com>
2025-11-06 22:10:38 -0700

    Nov 6 Work 2

--- Project/assignment_part3/Networks.py
+++ Project/assignment_part3/Networks.py
@@ -8,9 +8,9 @@ class Action_Conditioned_FF(nn.Module):
         # custom architecture
         super(Action_Conditioned_FF, self).__init__()  # Initialize nn module
         # Define layers
-        self.input_size = None  # Define input size
-        self.hidden_size = None  # Define hidden layer sizes
-        self.output_size = None  # Define output size
+        self.input_size = 5  # Define input size
+        self.hidden_size = 200  # Define hidden layer sizes
+        self.output_size = 1  # Define output size
 
         self.input_to_hidden = nn.Linear(self.input_size, self.hidden_size)
         self.hidden_to_output = nn.Linear(self.hidden_size, self.output_size)
@@ -22,12 +22,8 @@ class Action_Conditioned_FF(nn.Module):
         ## Input to Hidden Actions ##
         hidden = self.input_to_hidden(input)
         hidden = self.activation(hidden)
-        ## Input to Hidden Actions ##
-
         ## Hidden to Output Actions ##
-        hidden = self.hidden_to_output(hidden)
-        output = self.activation(hidden)
-        ## Hidden to Output Actions ##
+        output = self.hidden_to_output(hidden)
         return output
 
     def evaluate(self, model, test_loader, loss_function):
@@ -35,7 +31,15 @@ class Action_Conditioned_FF(nn.Module):
         # mind that we do not need to keep track of any gradients while evaluating the
         # model. loss_function will be a PyTorch loss function which takes as argument the model's
         # output and the desired output.
-        return loss
+
+        ## Prepare network output array ##
+        total_loss = 0
+        for input, label in test_loader:
+            model_output = model(input)
+            loss = loss_function(model_output, label)
+            total_loss += loss.item()
+        ## Prepare desired output array ##
+        return total_loss
 
 
 def main():
